74417d53400222e1bd2388dd514c713a5683f4e2
i-msy <simc2644@gmail.com>
2020-04-29 23:39:07 +0900

    make title and set background color

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -7,14 +9,24 @@ import javax.swing.SwingConstants;
 public class Calculator {
 	public Calculator() {
 		JFrame frame = new JFrame("학점 계산기");
-		JLabel title = new JLabel("학 점 계 산 기 ");
+		JLabel title = new JLabel();
 
 		Dimension dim = new Dimension(500, 600); // 프레임 사이즈 지정
-		frame.add(title);
 
-		title.setHorizontalAlignment(SwingConstants.TOP);
 		frame.setLocation(200, 400); // 창의 위치 설정
 		frame.setPreferredSize(dim);
+		title.setText("학점계산기");
+		title.setVerticalAlignment(SwingConstants.TOP); // 수직 정렬
+		title.setHorizontalAlignment(SwingConstants.CENTER); // 수평 정렬
+
+		Font font = new Font("AppleSDGothicNeoL00", Font.BOLD, 30);
+		title.setFont(font);
+
+		title.setOpaque(true); // background색 적용하기 위해 true로 설정해야 함
+		Color background = new Color(223, 215, 187);
+		title.setBackground(background);
+
+		frame.add(title);
 		frame.pack();
 		frame.setVisible(true);
 
